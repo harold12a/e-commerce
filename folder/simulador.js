0c7d3b1575caece3a59514c683390f0184fa11cc
harold alzate <haroldalzate99@gmail.com>
2022-10-13 14:08:55 -0500

    segunda entrega e-commerce

--- folder/simulador.js
+++ folder/simulador.js
@@ -1,96 +1,111 @@
 // harold alzate
 let opciones = "opciones de frutas\n";
 
-opciones +=" 1- BANANO $5.00 \n";
-opciones +=" 2- GUAYABA $15.00\n";
-opciones +=" 3- SANDIA $40.00\n";
-opciones +=" 4- UVA $20.00\n";
-opciones +=" 5- MANGO $10.00\n";
-opciones +=" 6- MANZANA $30.00\n";
-opciones +=" 7- COMPRAR\n";
-opciones +=" 8- SALIR\n";
+opciones += "  BANANO $5.00 \n";
+opciones += "  GUAYABA $15.00\n";
+opciones += "  SANDIA $40.00\n";
+opciones += "  UVA $20.00\n";
+opciones += "  MANGO $10.00\n";
+opciones += "  MANZANA $30.00\n";
+opciones += " 1- COMPRAR\n";
+opciones += " 2- BUSCAR PRODUCTO \n";
+opciones += " 3- FINALIZAR COMPRA\n";
 
 let opc = 0;
 let condicion = true;
 
+class producto {
+    constructor(nombre, precio, cantidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.dispobible = true;
+    }
+    sumaIva() {
+        return this.precio * 0.19;
+    }
+    vender() {
+        this.dispobible = false;
+    }
+    total() {
+        return this.sumaIva() * this.cantidad
+    }
+
+}
+
+const Productos =[
+    {nombre:"banano", precio:5, cantidad:152 },
+    {nombre:"guayaba", precio:15, cantidad:250 },
+    {nombre:"sandia", precio:40, cantidad:150 },
+    {nombre:"uva", precio:20, cantidad:500 },
+    {nombre:"mango", precio:10, cantidad:200 },
+    {nombre:"manzana", precio:300, cantidad:100 }
+
+];
+
+
+
 
 while (condicion) {
-    opc =parseInt(prompt(opciones));
+    opc = parseInt(prompt(opciones));
 
-    switch(opc){
-        case 1: alert(' Has elegido la fruta banano');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 2: alert(' Has elegido la fruta guayaba ');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 3: alert(' Has elegido la fruta sandia ');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 4: alert(' Has elegido la opcion de uva ');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 5: alert(' Has elegido la fruta mango ');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 6: alert(' Has elegido la fruta manzana ');
-        opc = parseInt(prompt('ingrese la cantidad a llevar'));
-        if(opc >= 5){
-            alert('tienes un descuento 10%')
-        }else{
-            alert('no tienes ningun descuento con esta cantidad')
-        }
-        break;
-        case 7: 
-        function Precio(precioArticulo, cantidadArticulo, descuento, valorEnvio){
-    
+    switch (opc) {
+        case 1:
 
-            let descuentoArticulo = (precioArticulo * descuento) / 100;
-            let valorDescuento = precioArticulo - descuentoArticulo;
-        
-            return ( valorDescuento * cantidadArticulo ) + valorEnvio;
-            
-        }
-        
-        const valorEnvio = 0;
-        
-        let articulo = parseFloat(prompt('ingrese el valor del Articulo'));
-        let cantidad = parseFloat(prompt('ingrese la cantidad del Articulo'));
-        let descuento = parseFloat(prompt('ingrese tu cupon descuento'));
-        
-        let finalValor = Precio(articulo,cantidad,descuento, valorEnvio);
-        
-        alert('el valor de la compra es:' + finalValor + ' GRACIAS POR COMPRAR EN TIENDA ONLINE');
-        break;
-        case 8: alert('te esperamos pronto')
-        condicion = false;
-        break;
-        default: alert('esta opcion no esta disponible ingresa la opciones del "1" - "8" ')
+            const arrayProductos = [];
+            datos = " ";
+            do {
+                let datos = prompt('ingrese la fruta o salir para finalizar');
+                if (datos === "salir" || datos === "SALIR") {
+                    break;
+                } else {
+                    nombreP = datos;
+                    let precioP = prompt('ingrese el valor de la fruta');
+                    let cantidadP = prompt('ingrese la cantidad a llevar');
+
+                    arrayProductos.push(new producto(nombreP, precioP, cantidadP));
+
+                }
+            }
+            while (datos != "salir" || datos != "SALIR");
+
+            console.log(arrayProductos);
+
+            for (let producto of arrayProductos) {
+                console.log('\n')
+                console.log(" el producto ingresado es: " + producto.nombre);
+                console.log(" el producto tiene un precio de: " + producto.precio);
+                console.log(" cantidad del producto: " + producto.cantidad);
+                console.log(" precio  del producto final unitario es : " + producto.sumaIva());
+                console.log(' total de la  compra es :' + producto.total())
+
+            }
+
+
+            break;
+        case 2:
+            const ingresar = prompt('ingrese el producto que quiere buscar');
+
+            const productoIngresado = Productos.filter(producto => producto.nombre.includes(ingresar));
+            console.log(productoIngresado);
+            console.log('-----');
+            console.log('PRODUCTO INGRESADO PARA LA BUSQUEDA');
+
+            for (let producto of productoIngresado) {
+                console.log('\n')
+                console.log(" el producto ingresado es: " + producto.nombre);
+                console.log(" el producto tiene un precio de: " + producto.precio + " pesos x kg");
+                console.log(" cantidad del producto: " + producto.cantidad);
+            }
+
+
+            break;
+        case 3: alert('te esperamos pronto; si relizaste una compra ve a console para ver el resultado')
+            condicion = false;
+            break;
+        default: alert('esta opcion no esta disponible ingresa la opciones del "1" - "3" ')
     }
 }
-    
+
+
+
