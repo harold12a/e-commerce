f9e24d703f13ecf7588ecb0c51f6d1809f233446
harold alzate <haroldalzate99@gmail.com>
2022-10-13 19:49:15 -0500

    segunda entrega e-commerce

--- folder/simulador.js
+++ folder/simulador.js
@@ -1,4 +1,5 @@
 // harold alzate
+
 let opciones = "opciones de frutas\n";
 
 opciones += "  BANANO $5.00 \n";
@@ -33,25 +34,21 @@ class producto {
 
 }
 
-const Productos =[
-    {nombre:"banano", precio:5, cantidad:152 },
-    {nombre:"guayaba", precio:15, cantidad:250 },
-    {nombre:"sandia", precio:40, cantidad:150 },
-    {nombre:"uva", precio:20, cantidad:500 },
-    {nombre:"mango", precio:10, cantidad:200 },
-    {nombre:"manzana", precio:300, cantidad:100 }
+const Productos = [
+    { nombre: "banano", precio: 5, cantidad: 152 },
+    { nombre: "guayaba", precio: 15, cantidad: 250 },
+    { nombre: "sandia", precio: 40, cantidad: 150 },
+    { nombre: "uva", precio: 20, cantidad: 500 },
+    { nombre: "mango", precio: 10, cantidad: 200 },
+    { nombre: "manzana", precio: 300, cantidad: 100 }
 
 ];
-
-
-
 
 while (condicion) {
     opc = parseInt(prompt(opciones));
 
     switch (opc) {
         case 1:
-
             const arrayProductos = [];
             datos = " ";
             do {
@@ -64,9 +61,9 @@ while (condicion) {
                     let cantidadP = prompt('ingrese la cantidad a llevar');
 
                     arrayProductos.push(new producto(nombreP, precioP, cantidadP));
-
                 }
             }
+
             while (datos != "salir" || datos != "SALIR");
 
             console.log(arrayProductos);
@@ -80,9 +77,8 @@ while (condicion) {
                 console.log(' total de la  compra es :' + producto.total())
 
             }
-
-
             break;
+
         case 2:
             const ingresar = prompt('ingrese el producto que quiere buscar');
 
@@ -97,12 +93,12 @@ while (condicion) {
                 console.log(" el producto tiene un precio de: " + producto.precio + " pesos x kg");
                 console.log(" cantidad del producto: " + producto.cantidad);
             }
-
-
             break;
+
         case 3: alert('te esperamos pronto; si relizaste una compra ve a console para ver el resultado')
             condicion = false;
             break;
+
         default: alert('esta opcion no esta disponible ingresa la opciones del "1" - "3" ')
     }
 }
